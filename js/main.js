34b5929d711aa33c73abe270caee482c778e564b
heejin <seokhj0311@naver.com>
2025-11-03 11:35:59 +0900

    25.11.03 작업 완료

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    // $(".ham_menu").click(function(){
+    //     $(".sub_menu_wrap").addClass("active");
+    // });
+
+    // $(".close_btn").click(function(){
+    //     $(".sub_menu_wrap").removeClass("active");
+    // });
+
+    $(".ham_menu").click(function(){
+        $(".sub_menu_wrap").css({
+            right: 0
+        });
+    });
+
+    $(".close_btn").click(function(){
+        $(".sub_menu_wrap").css({
+            right: '-100%'
+        });
+    });
+
+
+
+
+});
